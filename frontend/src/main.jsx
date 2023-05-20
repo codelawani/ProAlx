7d75e131c7e5c1523fea0c77b6a49dd6593991bc
Efezino Idisi <enduranceidisi13@gmail.com>
2023-05-21 00:40:11 +0100

    set up basic routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+//import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx';
+import ErrorPage from './pages/error/ErrorPage.jsx';
+import './index.css';
+import Dashboard from './pages/dashboard/Dashboard.jsx';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		errorElement: <ErrorPage />,
+	},
+	{
+		path: 'dashboard',
+		element: <Dashboard />,
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<RouterProvider router={router} />
+);
