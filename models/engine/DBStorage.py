8310820fbb6a16831b3f370eaa3663999188513d
Obihai <obichi@coseo.com>
2023-05-26 18:31:16 +0100

    updated the `all` method in DBStorage

--- models/engine/DBStorage.py
+++ models/engine/DBStorage.py
@@ -24,9 +24,20 @@ class DBStorage:
         # Create the database tables if they do not exist
         Base.metadata.create_all(self.engine)
 
-    def all(self):
+    def all(self, cls=None):
         """Returns all objects in the database"""
-        return self.session.query(Base).all()
+        from models.user import User
+        from models.cohort import Cohort
+
+        result = {}
+        for model in (User, Cohort):
+            if cls and model != cls:
+                continue
+            query_result = self.session.query(model).all()
+            for obj in query_result:
+                key = f"{obj.__class__.__name__}.{obj.id}"
+                result[key] = obj
+        return result
 
     def new(self, obj):
         try:
