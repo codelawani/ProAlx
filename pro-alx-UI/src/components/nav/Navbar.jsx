15db6a1560f5093f33abf767fa6dcf1e044bdfa6
Efezino <enduranceidisi13@gmail.com>
2023-05-28 10:12:51 +0100

    Revert "Feat/ext api"

--- pro-alx-UI/src/components/nav/Navbar.jsx
+++ pro-alx-UI/src/components/nav/Navbar.jsx
@@ -1,27 +1,24 @@
 import { Link } from 'react-router-dom';
-// import Login from '../login/Login';
+import Login from '../login/Login';
 import styles from './navbar.module.css';
 import PropTypes from 'prop-types';
-import LoginWithGithub from '../login/GithubLogin';
 
-const Navbar = ({ isLoggedIn, setIsLoggedIn, setIsLoading }) => {
-  return (
-    <nav className={styles.nav_menu}>
-      <ul className={styles.list}>
-        <li className={styles.list_items}>
-          <Link to='/'>About</Link>
-        </li>
-      </ul>
-      {/* <Login setUser={setUser} setIsLoading={setIsLoading} /> */}
-      <LoginWithGithub isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} setIsLoading={setIsLoading} />
-    </nav>
-  );
+const Navbar = ({ setUser, setIsLoading }) => {
+	return (
+		<nav className={styles.nav_menu}>
+			<ul className={styles.list}>
+				<li className={styles.list_items}>
+					<Link to={'/'}>About</Link>
+				</li>
+			</ul>
+			<Login setUser={setUser} setIsLoading={setIsLoading} />
+		</nav>
+	);
 };
 
 export default Navbar;
 
 Navbar.propTypes = {
-  isLoggedIn: PropTypes.bool,
-  setIsLoggedIn: PropTypes.func,
-  setIsLoading: PropTypes.func
+	setUser: PropTypes.func,
+	setIsLoading: PropTypes.func,
 };
--- pro-alx-UI/src/main.jsx
+++ pro-alx-UI/src/main.jsx
@@ -1,3 +1,4 @@
+//import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
@@ -6,17 +7,17 @@ import './index.css';
 import Dashboard from './pages/dashboard/Dashboard.jsx';
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />
-  },
-  {
-    path: 'dashboard',
-    element: <Dashboard />
-  }
+	{
+		path: '/',
+		element: <App />,
+		errorElement: <ErrorPage />,
+	},
+	{
+		path: 'dashboard',
+		element: <Dashboard />,
+	},
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+	<RouterProvider router={router} />
 );
