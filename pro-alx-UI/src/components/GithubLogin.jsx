92c0f73292886aee7678bcf1a3126386505fcc42
Efezino Idisi <enduranceidisi13@gmail.com>
2023-06-09 18:26:03 +0100

    remove unused

--- pro-alx-UI/src/components/GithubLogin.jsx
+++ pro-alx-UI/src/components/GithubLogin.jsx
@@ -11,60 +11,58 @@ const GITHUB_AUTH_URL = 'https://github.com/login/oauth/authorize';
 const SCOPE = 'read:user';
 
 const LoginWithGithub = ({ style = 'text-white', handleClick = () => {} }) => {
-  const navigate = useNavigate();
-  const { isLoggedIn, setIsLoggedIn, updateLoading, setUser } = useUser();
-  const handleAuth = () => {
-    const authUrl =
+	const navigate = useNavigate();
+	const { isLoggedIn, setIsLoggedIn, updateLoading, setUser } = useUser();
+	const handleAuth = () => {
+		const authUrl =
 			GITHUB_AUTH_URL +
 			`?client_id=${CLIENT_ID}` +
 			'&response_type=code' +
 			`&scope=${SCOPE}`;
-    window.location.assign(authUrl);
-  };
-  const clearUser = () => {
-    localDataMgr.clear();
-    setIsLoggedIn(false);
-    updateLoading(false);
-    setUser('');
-    navigate('/');
-  };
-  const handleLogout = () => {
-    updateLoading(true);
-    clearUser();
-  };
+		window.location.assign(authUrl);
+	};
+	const clearUser = () => {
+		localDataMgr.clear();
+		setIsLoggedIn(false);
+		updateLoading(false);
+		setUser('');
+		navigate('/');
+	};
+	const handleLogout = () => {
+		updateLoading(true);
+		clearUser();
+	};
 
-  const handleButtonEvent = () => {
-    if (isLoggedIn) {
-      handleLogout();
-    } else {
-      handleAuth();
-    }
-    handleClick();
-  };
+	const handleButtonEvent = () => {
+		if (isLoggedIn) {
+			handleLogout();
+		} else {
+			handleAuth();
+		}
+		handleClick();
+	};
 
-  return (
-    <Button
-      handleClick={handleButtonEvent}
-      value={
-				isLoggedIn
-				  ? (
-  <span className='flex items-center gap-2 uppercase font-mono text-sm'>
-    <BiLogOut className='text-[1.5rem]' />
-    Signout
-  </span>
-				    )
-				  : (
-				  'Sign in with Github'
-				    )
+	return (
+		<Button
+			handleClick={handleButtonEvent}
+			value={
+				isLoggedIn ? (
+					<span className='flex items-center gap-2 uppercase font-mono text-sm'>
+						<BiLogOut className='text-[1.5rem]' />
+						Signout
+					</span>
+				) : (
+					'Sign in with Github'
+				)
 			}
-      style={style}
-    />
-  );
+			style={style}
+		/>
+	);
 };
 
 export default LoginWithGithub;
 
 LoginWithGithub.propTypes = {
-  style: PropTypes.string,
-  handleClick: PropTypes.func
+	style: PropTypes.string,
+	handleClick: PropTypes.func,
 };
